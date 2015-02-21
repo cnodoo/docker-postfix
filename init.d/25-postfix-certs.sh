3254bb2c2d83853f5935ad25987b41deab276a82
dawi2332 <dawi2332@gmail.com>
2015-02-21 22:13:14 +0100

    Fixed init.d scripts

--- init.d/25-postfix-certs.sh
+++ init.d/25-postfix-certs.sh
@@ -7,10 +7,10 @@ cert=/etc/ssl/certs/postfix.pem
 key=/etc/ssl/private/postfix.key
 prefix=/x44.email/containers/$CONTAINER/postfix/
 
-getfile $prefix/ssl-cert.pem $cert \
+getfile $prefix/ssl-cert.pem $key \
     && chmod 640 $key \
     && chgrp ssl-cert $key \
     && postconf -e smtpd_tls_key_file=$key
 
-getfile $prefix/ssl-cert.key $key \
+getfile $prefix/ssl-cert.key $cert \
     && postconf -e smtpd_tls_cert_file=$cert
