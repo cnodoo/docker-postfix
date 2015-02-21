18b9fc1039abbf32f0db81557cb22352af2e8704
dawi2332 <dawi2332@gmail.com>
2015-02-21 11:26:01 +0100

    Fixed 50-rsyslog-remote.sh

--- init.d/50-rsyslog-remote.sh
+++ init.d/50-rsyslog-remote.sh
@@ -3,8 +3,7 @@
 rm /etc/rsyslog.d/50-default.conf
 
 REMOTE_HOST=`etcdctl get /x44.email/services/rsyslog/remotehost`
-if [[ ! -z $REMOTE_HOST ]]
-then
+if [ ! -z $REMOTE_HOST ]; then
     echo "rsyslog remote host: $REMOTE_HOST"
     echo "*.* $REMOTE_HOST" > /etc/rsyslog.d/50-remote.conf
 fi
