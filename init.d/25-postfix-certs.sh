d6bbf3ec2add0f66bfc5841af9c907c30b913090
dawi2332 <dawi2332@gmail.com>
2015-02-23 21:53:42 +0100

    Changed TLS ciphers, added support for ECDSA cert

--- init.d/25-postfix-certs.sh
+++ init.d/25-postfix-certs.sh
@@ -1,14 +1,24 @@
 #! /bin/sh
 set +e
 
-cert=/etc/ssl/certs/postfix.pem
-key=/etc/ssl/private/postfix.key
+rsacert=/etc/ssl/certs/smtp-rsa.pem
+rsakey=/etc/ssl/private/smtp-rsa.key
+ecdsacert=/etc/ssl/certs/smtp-ecdsa.pem
+ecdsakey=/etc/ssl/private/smtp-ecdsa.key
 prefix=/x44.email/containers/$CONTAINER/postfix/
 
-etcdctl get $prefix/ssl-cert.pem > $key 2>/dev/null \
-    && chmod 640 $key \
-    && chgrp ssl-cert $key \
-    && postconf -e smtpd_tls_key_file=$key
+etcdctl get $prefix/smtp-rsa.key > $rsakey 2>/dev/null \
+    && chmod 640 $rsakey \
+    && chgrp ssl-cert $rsakey \
+    && postconf -e smtpd_tls_key_file=$rsakey
 
-etcdctl get $prefix/ssl-cert.key > $cert 2>/dev/null \
-    && postconf -e smtpd_tls_cert_file=$cert
+etcdctl get $prefix/smtp-rsa.pem > $rsacert 2>/dev/null \
+    && postconf -e smtpd_tls_cert_file=$rsacert
+
+etcdctl get $prefix/smtp-ecdsa.key > $ecdsakey 2>/dev/null \
+    && chmod 640 $ecdsakey \
+    && chgrp ssl-cert $ecdsakey \
+    && postconf -e smtpd_tls_eckey_file=$ecdsakey
+
+etcdctl get $prefix/smtp-ecdsa.pem > $ecdsacert 2>/dev/null \
+    && postconf -e smtpd_tls_eccert_file=$ecdsacert
